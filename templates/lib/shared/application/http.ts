99d94415161fc77b3792c54206cefed72dc2f1c6
Alejandro CR <contacto@alejandrocr.co>
2026-07-08 17:13:08 -0500

    refactor: convert HttpResponseBody from class to interface and clean up structure

--- templates/lib/shared/application/http.ts
+++ templates/lib/shared/application/http.ts
@@ -1,48 +1,36 @@
-// Libraries
-
-// Same Layer
-
-// Lower Layers
-
-// Types
-
-// Constants
+/**
+ * @description
+ */
+export interface HttpRequest {}
 
 /**
  * @description
  */
-export class HttpResponseBody {
+export interface HttpResponse {}
+
+/**
+ * @description
+ */
+export interface HttpResponseBody {
     [property: string]: unknown
+    readonly data: Record<string, unknown> | null
+    readonly errors: string[] | null
+    readonly links: Record<string, URL> | null
+}
 
-    // public ATTRIBUTES
+/**
+ * @description
+ */
+export interface HttpRequestHandler {
+    handle(request: HttpRequest): HttpResponse | Promise<HttpResponse>
+}
 
-    // protected ATTRIBUTES
-
-    // private ATTRIBUTES
-
-    // public static ATTRIBUTES
-
-    // protected static ATTRIBUTES
-
-    // private static ATTRIBUTES
-
-    // Constructor, Getters, Setters
-
-    public constructor(
-        public readonly data: Record<string, unknown> | null = null,
-        public readonly errors: string[] | null = null,
-        public readonly links: Record<string, URL> | null = null
-    ) {}
-
-    // public METHODS
-
-    // protected METHODS
-
-    // private METHODS
-
-    // public static METHODS
-
-    // protected static METHODS
-
-    // private static METHODS
-} //:: class
+/**
+ * @description
+ */
+export interface HttpMiddleware {
+    process(
+        request: HttpRequest,
+        handler: HttpRequestHandler
+    ): HttpResponse | Promise<HttpResponse>
+}
