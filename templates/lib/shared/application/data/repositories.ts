9ea8e02774a281d8533ebce5960fb64284b64a94
Alejandro CR <contacto@alejandrocr.co>
2026-08-29 02:47:26 -0500

    refactor: update Repository class to include generic DataManager type and enhance transform method signature

--- templates/lib/shared/application/data/repositories.ts
+++ templates/lib/shared/application/data/repositories.ts
@@ -1,5 +1,4 @@
 import { type DataManager } from './managers.js'
-import { type DriverAdapter } from './drivers.js'
 
 type Generic = Record<string, unknown>
 
@@ -7,12 +6,12 @@ type Generic = Record<string, unknown>
  * @description Acts as an intermediary between plain source data and domain objects.
  * It transforms records into domain representations and can translate them back when needed.
  */
-export abstract class Repository<RawDataShape = Generic, EntityShape = Generic> {
+export abstract class Repository<RawDataShape = Generic, EntityShape = Generic, M extends DataManager<RawDataShape> = DataManager<RawDataShape>> {
     [property: string]: unknown
 
     public constructor(
-        public readonly driver: DriverAdapter<DataManager<RawDataShape>>
+        public readonly manager: M
     ) {}
 
-    protected abstract transform(data: RawDataShape): EntityShape
+    protected abstract transform(data: RawDataShape, ...args: unknown[]): EntityShape
 } //:: class
