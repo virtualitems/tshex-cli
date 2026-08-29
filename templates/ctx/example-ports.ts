d363a1587a970d6ab06245ff326de71a959d51ed
Alejandro CR <contacto@alejandrocr.co>
2026-08-28 23:37:41 -0500

    style: update comment in example-ports.ts for clarity

--- templates/ctx/example-ports.ts
+++ templates/ctx/example-ports.ts
@@ -1,4 +1,5 @@
 // Ports are exports from the context root level
+// you can delete this file and create your own ports file in the context root level
 
 export function example(): void {
     // ...
